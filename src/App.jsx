7195f10cadf07071110984933c0199f04bf66bb9
Samit-Bains <samitpalbains@gmail.com>
2025-08-27 16:06:14 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+
+  	return (
+		<>
+			<div style={{textAlign: 'center'}}>
+				<h1>
+					AllTrue.AI Assignment
+				</h1>
+				<h2>
+					Samit Bains
+				</h2>
+				<h3>
+					August 27, 2025
+				</h3>
+			</div>
+		</>
+	)
+}
+
+export default App
